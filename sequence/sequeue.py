2d32f2cfd16f2d61bfe2b9b78b5d78b1a9468ece
alex,wang <532500648@qq.com>
2019-12-14 16:20:03 +0800

    add the simple python scripts

---
+++ sequence/sequeue.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.6
+str="123456"
+print(str)
+
+str=str+"!!!"
+print(str)
+
+str2="Alex"
+
+print(len(str2))
+
+str3=str2*10
+print(str3)
+
+
+str4='ertgdgd'
+exist="e" in str4
+print(exist)
+
+dir(str4)
+print(type(str4))
+print("t" in str4)
+print("x" in str4)
+print(max(str4))
+print(min(str4))
+
+
+a="Hello"
+b="World"
+
+#print(cmp(a,b))
