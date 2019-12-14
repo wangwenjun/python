8a825cbf2bb49a387978d661026ae8c5dfd22a92
alex,wang <532500648@qq.com>
2019-12-14 22:02:24 +0800

    add the variables.py script for python integer and string type variable

---
+++ variables.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.6
+
+print("-"*10)
+i = 7
+print(i," type is ",type(i))
+print("-"*10)
+
+i = 5+4+6+7
+print(i)
+print("-"*10)
+
+i = 5*5
+print(i)
+print("-"*10)
+
+
+i=100-445
+print(i)
+print("-"*10)
+
+
+name="Alex Wang"
+print(name,"name type is: ", type(name))
+
+name='Alex Wang'
+print(name,"name type is: ", type(name))
+
+
+name='''Alex Wang'''
+print(name,"name type is: ", type(name))
+print("-"*10)
+
+
