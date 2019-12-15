f07e6ae8c582e391f048f36d929a0631605baa60
alex,wang <532500648@qq.com>
2019-12-15 23:23:50 +0800

    add the string method script

---
+++ chapter02/string-method.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print("sdfsfs".capitalize())
+
+print("HELLO".lower())
+
+print("hello".upper())
+
+print("Hello".startwith("H"))
+print("Hello".startwith("He"))
+
+print("Hello".endwith("o"))
+print("Hello".endwith("lo"))
+
