16bcb96b2f8a1464b334169c4c949fa798278894
alex,wang <532500648@qq.com>
2019-12-15 16:00:03 +0800

    organized package structure and added the chapter02 folder

---
+++ chapter02/data-type.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import math
+
+i = 4242
+x=-34535
+print(i,type(i))
+print(x,type(x))
+
+
+large_integer=34567898327463893216847532149022563647754227885439016662145553364327889985421
+print(type(large_integer),large_integer)
+
+
+f=23423.234
+print(f,type(f))
+
+print(math.pi,type(math.pi))
+print(math.e,type(math.e))
+
+print(i,float(i))
+
