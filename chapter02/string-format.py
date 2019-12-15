86026b902290f150fe9351e4368b17a580a25217
alex,wang <532500648@qq.com>
2019-12-15 23:19:35 +0800

    add string format very similar scala s'' syntx

--- chapter02/string-format.py
+++ chapter02/string-format.py
@@ -16,4 +16,10 @@ print(str)
 i=10
 print(f"i equal {i} and add 10 equal to {i+10}")
 
+str="{} learning python and {} length is:{}"
+print(str.format(name,name,len(name)))
+x=10
+y=20
+print("{}+{}={}".format(x,y,x+y))
 
+print("%d+%d=%d"%(x,y,x+y))
