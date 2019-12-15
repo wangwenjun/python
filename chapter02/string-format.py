b582b23ee411f8cade1beb9bb3c38e373f845f6e
alex,wang <532500648@qq.com>
2019-12-15 23:14:49 +0800

    add string format very similar scala s'' syntx

---
+++ chapter02/string-format.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import math
+
+str=f"math.pi={math.pi}"
+print(str)
+
+name="Alex"
+
+str=f"{name} learning python and {name} length is:{len(name)}"
+
+print(type(str))
+print(str)
+
+
+i=10
+print(f"i equal {i} and add 10 equal to {i+10}")
+
+
