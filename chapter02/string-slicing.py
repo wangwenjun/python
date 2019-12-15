a4fb21617c576a55874f8fa7943f7937b02cecae
alex,wang <532500648@qq.com>
2019-12-15 23:07:23 +0800

    add the string slicing exercise

---
+++ chapter02/string-slicing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+str="pythonhello"
+
+print(str[1:5]) #ytho
+
+print(str[5:100]) #nhello
+
+print(str[-5:-1]) #hell
+
+#print(str[-5,11])
+
+print(str[1:]) #ythonhello
+
+print(str[:7]) #python
+
+print(len(str))
+
+print(len(""))
+print(len("")==0)
+
