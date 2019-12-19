376c63cbe07700855dc1c117c8774c1106830124
alex,wang <532500648@qq.com>
2019-12-19 22:49:59 +0800

    add the escape python scripts

---
+++ chapter02/string-escape.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print("Hello\nWorld")
+print("Hello\"World")
+print('Hello\'World')
+print("hello\axxx")
+print("hello\bxxx")
+print("hello\fxxx")
+print("dfsdf\tdfsdfs")
+print("sdfsdfs\vsdfsfsd")
+print("sdfsdf\000234234")
+print("sdfsfs\x345453")
+
