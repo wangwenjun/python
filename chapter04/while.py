4d5e243159ec5e0f969948e69077ff4ad1496b19
alex,wang <532500648@qq.com>
2019-12-21 23:42:37 +0800

    the while: else: statement example

---
+++ chapter04/while.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# A while statement allows you to execute a block of code repeatedly
+i=10
+while i>0:
+    print(f"i is bigger than 0: {i}")
+    i-=1
+else:
+    print("i is less or equal 0")
