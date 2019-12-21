a7cb3bf8e0b4fc2b8b3f69e1a7b9ba2404ea75eb
alex,wang <532500648@qq.com>
2019-12-21 16:34:30 +0800

    add the python logical operator in boolean variable

---
+++ chapter03/logical.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+fruits=["Apple","Orange","Banana","Mongoes"]
+
+print("Apple" in fruits and "Banana" in fruits)
+
+print("Apple" in fruits or "tomotos" in fruits)
+
+print("Apple" in fruits and "tomotos" in fruits)
+
+print("Apple" in fruits and "tomotos" not in fruits)
+
+print(not ("Apple" in fruits and "Banana" in fruits))
