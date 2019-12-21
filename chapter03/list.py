e3513fa7f67f4d0c4784a61c88b6a949d21e8b7b
alex,wang <532500648@qq.com>
2019-12-21 15:09:51 +0800

    the list data type simple demo

--- chapter03/list.py
+++ chapter03/list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 digits=[1,2,3,4,5,6,7,8]
-print(digits)
+print(digits,"len=",len(digits))
 
 letters=['a','b',"c",'''d''']
-print(letters)
+print(letters,"len=",len(letters))
 
 mixed=[1,2,3,'a','b',[1,2,3,"a"]]
-print(mixed)
+print(mixed,"len=",len(mixed))
+
+
 
