db260907c50e7770a8943d9dbd2832651e485aeb
alex,wang <532500648@qq.com>
2019-12-22 15:18:47 +0800

    add the range example

--- chapter04/range.py
+++ chapter04/range.py
@@ -2,5 +2,17 @@
 print("range simple example.")
 
 r=range(10)
-print(r)
+print(r,type(r))
+
+l=list(r)
+print(l,type(l))
+
+
+r=range(0,10,3)
+print(r,list(r))
+
+for i in range(1,10,2):
+    print(i)
+else:
+    print("last element:",i)
 
