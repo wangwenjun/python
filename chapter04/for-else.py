b12d0a2b8ae5c9959dab5549d59e24c73e7b4b14
alex,wang <532500648@qq.com>
2019-12-22 15:07:14 +0800

    the for else statement and range

---
+++ chapter04/for-else.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+print("for loop the string")
+
+list=[1,2,3,4,5,6,7,8,9]
+for e in list:
+    print(e,"square is:",e**2)
+else:
+    print("======================")
---
+++ chapter04/range.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+print("range simple example.")
+
+r=range(10)
+print(r)
+
