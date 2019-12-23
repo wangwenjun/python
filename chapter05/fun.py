695315006254633285684447cf35fc1ffa75cc12
alex,wang <532500648@qq.com>
2019-12-23 22:03:21 +0800

    main function

---
+++ chapter05/fun.py
@@ -0,0 +1,6 @@
+def accumulate(list):
+    result=0
+    for e in list:
+        result+=e
+    return result
+print(__name__)
---
+++ chapter05/main.py
@@ -0,0 +1,10 @@
+import fun
+
+list=[1,2,3,4,5,6,7,8,9,10]
+
+def main():
+    result=fun.accumulate(list)
+    print(list,"sum is:",result)
+
+if __name__ == "__main__":
+    main()
