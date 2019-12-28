68b5b69dab8dd9819c8aadad93faecaf5b8e860f
alex,wang <532500648@qq.com>
2019-12-28 17:10:54 +0800

    create the tuple example 2

---
+++ chapter07/create-tuple-2.py
@@ -0,0 +1,10 @@
+t=1,2,3,4
+print(t,len(t),type(t))
+t=(1,2,3,4)
+print(t,len(t),type(t))
+t=(1,2),3,4
+print(t,len(t),type(t))
+
+
+t=1,2,3,"Hello",[1,2,3,4,5],(1,2,3),[1,2,3,("sdf","ryr")]
+print(t,len(t),type(t))
