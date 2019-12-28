acb2d81c2458f741fb95435192d7fd0bc4b589ca
alex,wang <532500648@qq.com>
2019-12-28 17:06:01 +0800

    the tuple create example

---
+++ chapter07/create-tuple.py
@@ -0,0 +1,16 @@
+pets=("dogs","cats","tiggers")
+print(pets,type(pets),id(pets))
+
+t=(1,2,3,4)
+print(t,type(t),id(t))
+
+t=(1,2,3,4,"Hello","World",(1,2,3,"a"),[1,2,3])
+print(t,len(t))
+
+
+t=1,2,3,4,5,6
+print(t,type(t),len(t))
+
+# mulitple assign
+a,b=1,2
+print(a,b)
