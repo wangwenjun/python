44ac62d52ec36f61e02874504758f05655891b61
alex,wang <532500648@qq.com>
2019-12-30 23:41:20 +0800

    add the object-oriented programming add the object method example

---
+++ User.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self,name,age,address):
+        self.name=name
+        self.age=age
+        self.address=address
+
+
+    def fun(self):
+        print(__class__,"fun method.")
+
