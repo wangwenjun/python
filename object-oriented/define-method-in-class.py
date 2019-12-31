03a0a8adc23f0db8c256b08cbaba58473c03cbd8
alex,wang <532500648@qq.com>
2019-12-31 16:33:32 +0800

    define the method in class

---
+++ object-oriented/define-method-in-class.py
@@ -0,0 +1,24 @@
+class Person:
+
+    """
+        The person class construction method.
+        for example:
+            p=Person("Alex",22,"China")
+            print(p.name,p.age,p.address)
+
+    """
+    def __init__(self,name,age,address):
+        self.name=name
+        self.age=age
+        self.address=address
+
+
+    """
+        define the method in person class.
+    """
+    def greeting(self):
+        print("Hello ",self.age)
+
+
+    def __str__(self):
+        return f"Name:{self.name},Age:{self.age},Address:{self.address}"
